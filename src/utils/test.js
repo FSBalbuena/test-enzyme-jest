dd779f0cd61b59bf029584682160ad10f1dc4cd1
fsbalbuena <fsbalbuena@gmail.com>
2019-11-22 11:06:16 -0300

    how to test proptypes

--- src/utils/test.js
+++ src/utils/test.js
@@ -1,3 +1,6 @@
+import checkPropTypes from 'check-prop-types'
 
 
 export const findByTestAtrr=(wrapper,value)=>wrapper.find(`[data-test="${value}"]`)
+
+export const checkProps=(component,expectedProps)=>checkPropTypes(component.propTypes,expectedProps,"props",component.name)
