2c52eb825c7f4cc2839f7f4764dff6d4c807d10e
fsbalbuena <fsbalbuena@gmail.com>
2019-11-23 15:39:37 -0300

    checking test doesnt pass

--- src/components/button/button.test.js
+++ src/components/button/button.test.js
@@ -62,7 +62,7 @@ describe("Button component",()=>{
         it('handleClick props must be called when i click the button',()=>{
             let button=findByTestAtrr(wrapper,"post-button")
             button.simulate('click')
-            expect(spy.mock.calls.length).toBe(1)
+            expect(spy.mock.calls.length).toBe(3)
         })
     })
 
